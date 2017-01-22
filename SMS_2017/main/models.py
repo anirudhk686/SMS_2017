8ae5317579eea9deaa6d80d0d73936b835719e8c
Nischay Singh <bajwa.nischay@gmail.com>
2017-01-22 16:23:29 +0530

    Update models.py

--- SMS_2017/main/models.py
+++ SMS_2017/main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 start_money=1000000 #<complete> decide
 
 class Team(models.Model):
-	team_no = models.CharField(primary_key=True, max_length=3, help_text="Team Number") #For bitsians it will be last 3 digits of first idno
+	team_no = models.CharField(primary_key=True, max_length=5, help_text="Team Number") #For bitsians it will be last 3 digits of first idno
 	password = models.CharField(max_length=3) # Random generated 3 digit no.
 	id1 = models.CharField(max_length=12, default="", help_text="Paritcipant 1 ID") #idno of first participant. Format- 15059
 	id2 = models.CharField(max_length=12, default="",help_text="Paritcipant 2 ID") #idno of second participant
